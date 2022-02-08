ddf898000f36526106b1d665f53b6c0b0aed1979
Arthurjim <arthurjim013@gmail.com>
2022-02-07 23:20:35 -0600

    Add computed Properties

---
+++ src/components/Counter.vue
@@ -0,0 +1,37 @@
+<template>
+  <h2>Counter!</h2>
+  <p>{{count}}<sup>2</sup> ={{squareCounter}} </p>
+  <!-- <p>{{count}}<sup>2</sup> ={{squareCounter}} </p>
+  <p>{{count}}<sup>2</sup> ={{squareCounter}} </p>
+  <p>{{count}}<sup>2</sup> ={{squareCounter}} </p>
+  <p>{{count}}<sup>2</sup> ={{squareCounter}} </p> -->
+</template>
+
+
+
+<script>
+export default {
+    // name: 'Counter',
+
+    data(){
+        return{
+            count:5,
+        }  
+    },
+    methods:{
+       getSquareValue(){
+           return this.count * this.count;
+       }
+    },
+    computed:{
+        //Se guarda el valor en el cache y solo se llama una vez
+        squareCounter(){
+            return this.count * this.count;
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
